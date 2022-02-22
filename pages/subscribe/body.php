c2c7fd7c697ea8be2d10cd58202da1b1a16009b4
Chris Chasm <chris@chasm.solutions>
2022-02-22 12:47:20 -0700

    Added lead form

--- pages/subscribe/body.php
+++ pages/subscribe/body.php
@@ -1,89 +1,24 @@
 <?php require_once( trailingslashit( plugin_dir_path(__DIR__) ) . '/assets/nav.php') ?>
-<br><br><br>
+
 
 <section class="pb_section" data-section="about" id="section-about">
     <div class="container">
         <div class="row justify-content-md-center text-center mb-5">
             <div class="col-lg-7">
-                <h2 class="mt-0 heading-border-top font-weight-normal">What are Prayer Laps?</h2>
+                <h2 class="mt-0 heading-border-top font-weight-normal">We haven't launched yet!</h2>
                 <p>
-                    One prayer lap equals a complete series of community prayers covering every populated place in the world.
+                    We are collecting interest in our project. Leave us your email, so we can alert you when the project goes live (Spring 2022).
                 </p>
-
             </div>
         </div>
-        <div class="row">
+        <div class="row justify-content-md-center text-center mb-5">
             <div class="col-lg-7">
-                <div class="images right">
-                    <img class="img1 img-fluid" src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/pray-4770.jpg" alt="image">
-                    <img class="img2" src="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/images/dark-map.jpg" alt="image">
-                </div>
-            </div>
-            <div class="col-lg-5 pl-md-5 pl-sm-0">
-                <div id="exampleAccordion" class="pb_accordion" data-children=".item">
-                    <div class="item">
-                        <a data-toggle="collapse" data-parent="#exampleAccordion" href="#exampleAccordion1" aria-expanded="true" aria-controls="exampleAccordion1" class="pb_font-18">How it works</a>
-                        <div id="exampleAccordion1" class="collapse show" role="tabpanel">
-                            <p>
-                                We have identified 4,770 administrative divisions into which the countries of the world are divided. This website guides prayer warriors to pray over these specific locations.
-                            </p>
-                            <p>
-                                When our community prays for the "kingdom to come" over one of these divisions for one minute or more, then we mark that location as covered for that prayer lap.
-                            </p>
-                            <p>
-                                Once all the divisions of the world have been prayed for, then we close that prayer lap, start a new lap, and reset the master clock.
-                            </p>
-                            <p>
-                                We hope that our community completes each prayer lap faster than the previous. Can you imagine a global community praying forever place in the world every 10 minutes? We dream that.
-                            </p>
-                            <p>
-                                <br>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <a data-toggle="collapse" data-parent="#exampleAccordion" href="#exampleAccordion2" aria-expanded="false" aria-controls="exampleAccordion2" class="pb_font-18">Moravian 2.0</a>
-                        <div id="exampleAccordion2" class="collapse" role="tabpanel">
-                            <p>
-                                Inspired by the <a href="https://www.christianitytoday.com/history/issues/issue-1/prayer-meeting-that-lasted-100-years.html" target="_blank">Moravians</a>, who prayed non-stop for 100 years,
-                                we have crafted this website to help the church pray for the entire world in measurable units, as a community, and to know at the end when we have finished ... and are ready to start
-                                another lap.
-                            </p>
-                            <p>
-                                Once every location in the world has been prayed for, we finish a lap. The prayer map resets and we try to pray over the world again
-                                ... maybe faster.
-                            </p>
-                            <p>
-                                <br>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <a data-toggle="collapse" data-parent="#exampleAccordion" href="#exampleAccordion3" aria-expanded="false" aria-controls="exampleAccordion3" class="pb_font-18">Extraordinary Prayer for Movement</a>
-                        <div id="exampleAccordion3" class="collapse" role="tabpanel">
-                            <p>Based upon research, extraordinary prayer is found at the root of all modern disciple multiplying movements.</p>
-                            <p>Prayer.Global seeks to encourage in creative ways extraordinary prayer for the fulfillment of the Great Commission in our generation.</p>
-                            <p>
-                                <br>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <a data-toggle="collapse" data-parent="#exampleAccordion" href="#exampleAccordion4" aria-expanded="false" aria-controls="exampleAccordion4" class="pb_font-18">Historic Moment</a>
-                        <div id="exampleAccordion4" class="collapse" role="tabpanel">
-                            <p>Never before in the history of the human race have we been able to coordinate a global community of people to pray for the kingdom to come on earth as it is in heaven IN REALTIME!</p>
-                            <p>It has always pleased the Lord to use current technologies for the advance of his kingdom. Remembering, written language was a technology advancement. The printing press to distribute bibles was a technology advancement.</p>
-                            <p>God is now using global travel and global communication to take the gospel to the ends of the earth. Our heart is to add to that globally coordinated prayer for the kingdom.</p>
-                            <p>
-                                <br>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
+                <p>
+                    <iframe src="https://prayer.global/wp-content/plugins/disciple-tools-webform/public/form.php?token=dc9285f17f4e9f670bcf91f00489b7ed" style="width:100%;height:400px;" frameborder="0"></iframe>
+                </p>
             </div>
         </div>
     </div>
 </section>
 <!-- END section -->
-<iframe src="https://prayer.global/wp-content/plugins/disciple-tools-webform/public/form.php?token=dc9285f17f4e9f670bcf91f00489b7ed" style="width:100%;height:750px;" frameborder="0"></iframe>
+
