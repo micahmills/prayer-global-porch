fea2ce488df5d38cd3103db8da864ae27c32712e
Chris Chasm <chris@chasm.solutions>
2022-06-29 14:36:01 -0600

    Added social images

--- pages/media/magic-media.php
+++ pages/media/magic-media.php
@@ -70,7 +70,6 @@ class Prayer_Global_Porch_Media extends DT_Magic_Url_Base
         <link href="https://fonts.googleapis.com/css?family=Crimson+Text:400,400i,600|Montserrat:200,300,400" rel="stylesheet">
         <link rel="stylesheet" href="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/fonts/ionicons/css/ionicons.min.css">
         <link rel="stylesheet" href="<?php echo esc_url( trailingslashit( plugin_dir_url( __DIR__ ) ) ) ?>assets/basic.css?ver=<?php echo esc_attr( fileatime( trailingslashit( plugin_dir_path( __DIR__ ) ) . 'assets/basic.css' ) ) ?>" type="text/css" media="all">
-        <link rel="stylesheet" href="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>heatmap.css?ver=<?php echo esc_attr( fileatime( trailingslashit( plugin_dir_path( __FILE__ ) ) . 'heatmap.css' ) ) ?>" type="text/css" media="all">
         <?php
     }
 
@@ -81,7 +80,7 @@ class Prayer_Global_Porch_Media extends DT_Magic_Url_Base
     public function body(){
         require_once( trailingslashit( plugin_dir_path( __DIR__ ) ) . '/assets/nav.php' ) ?>
 
-        <section class="pb_section" style="height: 95vh;">
+        <section class="pb_section" >
             <div class="container">
                 <div class="row justify-content-md-center text-center mb-5">
                     <div class="col-lg-7">
@@ -91,33 +90,49 @@ class Prayer_Global_Porch_Media extends DT_Magic_Url_Base
                     </div>
                 </div>
                 <div class="row justify-content-md-center text-center mb-5">
+                    <div class="col-12 mb-3">
+                       <hr>
+                    </div>
                     <div class="col-12">
+                        <h2>Moravian Challenge Video</h2>
+                    </div>
+                    <div class="col-lg-3 p-3">
+                        Video<br>
+                        <div style="padding:56.3% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/715752828?h=d39d43cea8&amp;badge=0&amp;autopause=1&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="moravian-prayer-challenge"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+                    </div>
+                    <div class="col-lg-3 p-3">
+                        <p>
+                            Download Video
+                            <br><a href="https://player.vimeo.com/progressive_redirect/download/715752828/container/44c88492-73ba-4789-9e48-c88316194653/c2189175/moravian-prayer-challenge%20%28360p%29.mp4?expires=1656520543&loc=external&signature=35931e268af8c01f4c82c771b47d2b3393fca9f000dc62ff89afe0f6bb6ac1a5">Low Quality (360p - 10mg)</a>
+                            <br><a href="https://player.vimeo.com/progressive_redirect/download/715752828/container/44c88492-73ba-4789-9e48-c88316194653/19ce9b8c-bc019d05/moravian-prayer-challenge%20%281080p%29.mp4?expires=1656520543&loc=external&signature=5ee3781d3107607bbb25b556a0c87cacfa4126412d3c2e9412b2db2f9366c18d">HD Download (1080p - 31mg)</a>
+                            <br><a href="https://player.vimeo.com/progressive_redirect/download/715752828/container/44c88492-73ba-4789-9e48-c88316194653/538db78d/moravian-prayer-challenge%20%282160p%29.mp4?expires=1656520543&loc=external&signature=2e0906096aec000bd78276f5bbde85f9e9c9ad7d627c4c92bfa14e58335faea9">4k Download (2160p - 74mg)</a>
+                        </p>
+                    </div>
+                    <div class="col-lg-3 p-3">
+                        <p>
+                            Embed Code<br>
+                            <textarea style="font-size: .6em; height: 150px; width: 100%;" readonly><div style="padding:56.3% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/715752828?h=d39d43cea8&amp;badge=0&amp;autopause=1&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="moravian-prayer-challenge"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script></textarea>
+                        </p>
+                    </div>
+                </div>
+                <div class="row justify-content-md-center text-center mb-5">
+                    <div class="col-12">
+                        <hr>
                         <h2>Social Promotion</h2>
                     </div>
-                    <div class="col-lg-3 p-3">
+                    <div class="col-lg-6 p-3">
                         <p>
-                            <img src="https://place-hold.it/300" class="img-thumbnail" />
-                            <br><a href="#">Download</a>
+                            <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) )  ?>firestarters-wanted-2.jpg" class="img-thumbnail" />
+                            <br><a href="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) )  ?>firestarters-wanted-2.jpg">Download</a>
                         </p>
                     </div>
                     <div class="col-lg-3 p-3">
                         <p>
-                            <img src="https://place-hold.it/300" class="img-thumbnail" />
-                            <br><a href="#">Download</a>
+                            <img src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) )  ?>dont-fear-the-dark.jpg" class="img-thumbnail" />
+                            <br><a href="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) )  ?>dont-fear-the-dark.jpg">Download</a>
                         </p>
                     </div>
-                    <div class="col-lg-3 p-3">
-                        <p>
-                            <img src="https://place-hold.it/300" class="img-thumbnail" />
-                            <br><a href="#">Download</a>
-                        </p>
-                    </div>
-                    <div class="col-lg-3 p-3">
-                        <p>
-                            <img src="https://place-hold.it/300" class="img-thumbnail" />
-                            <br><a href="#">Download</a>
-                        </p>
-                    </div>
+                    
                 </div>
                 <div class="row justify-content-md-center text-center mb-5">
                     <div class="col-12">
