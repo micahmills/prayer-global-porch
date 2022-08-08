c97839c1aa320537fb2a632349799a269c41e21a
Chris Chasm <chris@chasm.solutions>
2022-08-08 17:30:09 -0600

    Create dashboard-top-menu.php

---
+++ support/dashboard-top-menu.php
@@ -0,0 +1,57 @@
+<?php
+
+class PG_DT_Dashboard {
+
+    private static $_instance = null;
+    public static function instance() {
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+    
+    public function __construct() {
+        $this->namespace = $this->context . "/v" . intval( $this->version );
+        add_filter( 'dt_front_page', [ $this, 'front_page' ] );
+
+        add_filter( 'desktop_navbar_menu_options', [ $this, 'nav_menu' ], 10, 1 );
+        add_filter( 'off_canvas_menu_options', [ $this, 'nav_menu' ] );
+
+        $url_path = dt_get_url_path();
+        add_action( "template_redirect", [ $this, 'redirect' ] );
+        if ( strpos( $url_path, 'porch_dashboard' ) !== false ) {
+            add_action( 'wp_enqueue_scripts', [ $this, 'scripts' ] );
+        }
+    }
+
+    public function redirect() {
+        $url = dt_get_url_path();
+        if ( strpos( $url, "porch_dashboard" ) !== false ) {
+            $plugin_dir = dirname( __FILE__ );
+            $path = $plugin_dir . '/template-dashboard.php';
+            status_header( 200 );
+            include( $path );
+            die();
+        }
+    }
+    
+    public function scripts() {
+      
+    }
+    
+    public function front_page( $page ) {
+        ?>
+        <iframe src="/" width="100%" height="95vh" ></iframe>
+        <?php
+    }
+
+    public function nav_menu( $tabs ) {
+        $tabs['dashboard'] = [
+            "link"  => site_url( '/dashboard/' ),
+            "label" => __( "Dashboard", "disciple-tools-dashboard" )
+        ];
+        return $tabs;
+
+    }
+}
+PG_DT_Dashboard::instance();
