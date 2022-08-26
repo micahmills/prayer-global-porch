d769af8177c34e75f22b21d96fc973df5e6c0088
Chris Chasm <chris@chasm.solutions>
2022-08-26 07:59:48 -0600

    Change link color

--- pages/assets/working-footer.php
+++ pages/assets/working-footer.php
@@ -3,9 +3,9 @@
         <div class="row" >
             <div class="col-md-3">
                 <p class="pb_font-14">
-                    Made with &#10084;&#65039; by <a class="white" href="https://gospelambition.org">Gospel Ambition</a>.<br>
-                    Powered by <a class="white" href="https://disciple.tools">Disciple.Tools</a>.<br>
-                    Part of the <a class="white" href="https://pray4movement.org">Pray4Movement network</a>.
+                    Made with &#10084;&#65039; by <a href="https://gospelambition.org">Gospel Ambition</a>.<br>
+                    Powered by <a href="https://disciple.tools">Disciple.Tools</a>.<br>
+                    Part of the <a href="https://pray4movement.org">Pray4Movement network</a>.
                 </p>
             </div>
             <div class="col-md-3">
